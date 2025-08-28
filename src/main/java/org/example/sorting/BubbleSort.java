d58be73e313abfd913abc830ef96d05982181336
0x0desha74 <mudesha74@gmail.com>
2025-08-28 04:05:58 +0300

    Bubble Sort

---
+++ src/main/java/org/example/sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package org.example.sorting;
+
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+//        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
