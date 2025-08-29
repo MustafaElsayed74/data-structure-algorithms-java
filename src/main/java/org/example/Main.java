c4863f932b291bf5475e515b8d3e45bc831d3e49
0x0desha74 <mudesha74@gmail.com>
2025-08-29 05:10:12 +0300

    Quick Sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.search.BinarySearch;
 import org.example.search.LinearSearch;
 import org.example.sorting.BubbleSort;
 import org.example.sorting.InsertionSort;
+import org.example.sorting.QuickSort;
 import org.example.sorting.SelectionSort;
 
 import java.util.Arrays;
@@ -17,7 +18,7 @@ public class Main {
 
         int[] nums = {4, 1, 2, 8, 11, 9, 10};
 
-        InsertionSort.sort(nums);
+        QuickSort.sort(nums,0,nums.length-1);
         for (int num : nums) {
             System.out.print(num + " ");
         }
---
+++ src/main/java/org/example/sorting/QuickSort.java
@@ -0,0 +1,33 @@
+package org.example.sorting;
+
+public class QuickSort {
+    public static void sort(int[] arr, int l, int h) {
+        if (l < h) {
+            int pi = partition(arr, l, h);
+            sort(arr, l, pi - 1);
+            sort(arr, pi + 1, h);
+        }
+
+    }
+
+    private static int partition(int[] arr, int l, int h) {
+
+        int pi = arr[h];
+        int i = l - 1;
+        for (int j = l; j < h; j++) {
+            if (arr[j] < pi) {
+                i++;
+                swap(arr,i, j);
+            }
+        }
+        swap(arr, i+1, h);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
