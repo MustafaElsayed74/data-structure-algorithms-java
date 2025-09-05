d89dc0c9c222501d89b8610890853abc2dcfd573
0x0desha74 <mudesha74@gmail.com>
2025-09-05 08:21:26 +0300

    Stack Impl using dynamic size array

---
+++ src/main/java/org/example/DataStructures/DynamicStack.java
@@ -0,0 +1,63 @@
+package org.example.DataStructures;
+
+public class DynamicStack {
+    int capacity = 2;
+    int stack[] = new int[capacity];
+    int top = 0;
+
+    public void push(int value) {
+        if (size() >= stack.length)
+            expand();
+        else {
+            stack[top] = value;
+            top++;
+        }
+    }
+
+    private void expand() {
+        int[] newStack = new int[capacity * 2];
+        int length = size();
+        System.arraycopy(stack, 0, newStack, 0, length);
+        stack = newStack;
+        capacity *= 2;
+    }
+
+    public void show() {
+        for (int i : stack)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+    public int pop() {
+        if (isEmpty())
+            System.out.println("Stack is empty");
+        int value = stack[--top];
+        stack[top] = 0;
+        shrink();
+        return value;
+    }
+
+    private void shrink() {
+        int length = size();
+        if (length < capacity / 4) {
+            capacity /= 2;
+
+            int[] newStack = new int[capacity];
+            System.arraycopy(stack, 0, newStack, 0, length);
+            stack = newStack;
+        }
+    }
+
+    public int peek() {
+        return stack[--top];
+    }
+
+    public int size() {
+        return top;
+
+    }
+
+    public boolean isEmpty() {
+        return top <= 0;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.DataStructures.DynamicStack;
 import org.example.DataStructures.LinkedList.LinkedList;
 import org.example.DataStructures.Stack;
 
@@ -8,28 +9,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Stack stack = new Stack();
+        DynamicStack stack = new DynamicStack();
 //        System.out.println(stack.isEmpty());
         stack.push(2);
+        stack.show();
+
         stack.push(4);
+
+        stack.show();
         stack.push(1);
+        stack.show();
         stack.push(8);
+        stack.show();
         stack.push(5);
-        stack.push(5);
-        //stack.push(7);
-//        stack.show();
-//        System.out.println();
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.isEmpty());
-        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
-//        System.out.println(stack.isEmpty());
-//        stack.show();
-//        System.out.println(stack.peek());
-//        System.out.println(stack.size());
+        stack.show();
+        stack.push(6);
+        stack.show();
+        stack.pop();
+        stack.show();
+        stack.pop();
+        stack.show();
+        stack.pop();
+        stack.show();
+
 
 
     }
